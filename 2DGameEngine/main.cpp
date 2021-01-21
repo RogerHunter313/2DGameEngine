c1e4754db0e08dcbb6449cb60cdab1e009198fb2
RogerHunter313 <73745537+RogerHunter313@users.noreply.github.com>
2021-01-21 17:59:23 -0600

    added glm, lua, some gui stuff still a starting point for creating a game with sdl and lua

--- 2DGameEngine/main.cpp
+++ 2DGameEngine/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 #include <SDL.h>
+#include <glm/glm.hpp>
+#include <imgui/imgui.h>
+#include <sol/sol.hpp>
 using namespace std;
 
 int main(int argc, char** argv) {
+	sol::state lua;
+	lua.open_libraries(sol::lib::base);
+
+	glm::vec2 velocity = glm::vec2(2.0, -1.0);
+
+	SDL_Init(SDL_INIT_EVERYTHING);
+	
+	cout << "Yay! Dependencies are working correctly!" << endl;
 
 	return 0;
 }
